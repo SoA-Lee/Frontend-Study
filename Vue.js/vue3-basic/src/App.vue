fb8dd7d90746c7747d403153c62e0010124bf421
SoA-Lee <llsa0927@naver.com>
2022-03-22 16:48:16 +0900

    Getter & Setter & Watch

--- Vue.js/vue3-basic/src/App.vue
+++ Vue.js/vue3-basic/src/App.vue
@@ -1,42 +1,52 @@
 <template>
-  <Fruits />
-  <h1 
-    v-once 
-    @click="add">{{ msg }}
+  <button @click="add">
+    ADD
+  </button>
+  <h1 @click="changeMessage">
+    {{  msg  }}
   </h1>
-  <h1 v-html="new"></h1>
-  <h1 v-bind:class="active">{{ active }}</h1>
-  <h1
-    :[attr]="'active'"
-    @[event]="add">
-    {{ msg }}
-  </h1>
+  <h1>{{  reversedMessage  }}</h1>
 </template>
 
 <script>
-import Fruits from '~/components/Fruits.vue'
-
 export default {
-  components: { Fruits },
   data() {
-      return {
-          msg: "Hello World",
-          new: "<div style=\"color: red;\">Hello!!</div>",
-          active: "active",
-          event: "click"
-      };
+    return {
+      // Getter, Setter
+      msg : 'Hello?!'
+    }
+  },
+  computed: { // 읽기 전용
+    // Getter
+    // reversedMessage() { // 캐싱 있음, 이미 연산한 값이 있다면 반복적으로 연산 X
+    //   return this.msg.split('').reverse().join('')
+    // }
+    // Getter, Setter
+    reversedMessage: {
+      get() {
+        return this.msg.split('').reverse().join('')
+      },
+      set(value) {
+        this.msg = value
+      }
+    }
+  },
+  watch: { // 데이터들의 변경사항 감시
+    msg() {
+      console.log('msg:', this.msg)
+    },
+    reversedMessage(newValue) {
+      console.log('reversedMessage:', newValue)
+    }
   },
   methods: {
-      add() {
-          this.msg += "!";
-       }
+    add() {
+      // this.reversedMessage += '?!'
+      this.reversedMessage += '?!'
+    },
+    changeMessage() {
+      this.msg = 'Good!'
+    }
   }
 }
 </script>
-
-<style scoped>
-.active {
-  color: royalblue;
-  font-size: 100px;
-}
-</style>
---
+++ Vue.js/vue3-basic/src/App1.vue
@@ -0,0 +1,42 @@
+<template>
+  <Fruits />
+  <h1 
+    v-once 
+    @click="add">{{ msg }}
+  </h1>
+  <h1 v-html="newd"></h1>
+  <h1 v-bind:class="active">{{ active }}</h1>
+  <h1
+    :[attr]="'active'"
+    @[event]="add">
+    {{ msg }}
+  </h1>
+</template>
+
+<script>
+import Fruits from '~/components/Fruits.vue'
+
+export default {
+  components: { Fruits },
+  data() {
+      return {
+          msg: "Hello World",
+          new: "<div style=\"color: red;\">Hello!!</div>",
+          active: "active",
+          event: "click"
+      };
+  },
+  methods: {
+      add() {
+          this.msg += "!";
+       }
+  }
+}
+</script>
+
+<style scoped>
+.active {
+  color: royalblue;
+  font-size: 100px;
+}
+</style>
